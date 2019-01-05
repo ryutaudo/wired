07c2521064fb73d28d2df4c8b4eb76b9562f62f7
Ryuta Udo <ryutaudo@gmail.com>
2019-01-05 16:28:19 +0900

    fixing react hooks with hot loading

--- src/components/App/Body/index.jsx
+++ src/components/App/Body/index.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 
-import List from './List';
-
 const Body = () => {
-  // const [posts, setPosts] = useState([{ url: 'test blog URL' }]);
   const [count, setCount] = useState(0);
 
   return (
     <div>
       <p>{`You clicked ${count} times`}</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button type="button" onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   );
 };
--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import { hot } from 'react-hot-loader/root';
+import { setConfig } from 'react-hot-loader';
+
 import Body from './Body';
+
+setConfig({ pureSFC: true });
 
 const App = () => (
   <div className="App">
@@ -7,4 +13,4 @@ const App = () => (
   </div>
 );
 
-export default App;
+export default hot(App);
